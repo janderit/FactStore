22ccd874a626f45faca8cc027adbd8c4fb3f7392
Philip Jander (Jander.IT) <jander@janso.de>
2014-02-12 18:23:21 +0100

    Nulleventstorage added to signal no-event-storage connected

---
+++ FactStore.Implementations/NullEventStorage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace JIT.FactStore
+{
+    /// <summary>
+    /// Missing event storage. Delivers empty "All" but throws on adding...
+    /// </summary>
+    public sealed class NullEventStorage : EventStorage
+    {
+        private NullEventStorage()
+        {
+        }
+
+        public static readonly NullEventStorage Singleton = new NullEventStorage();
+
+        public void Add(int commit, EventSet eventSet)
+        {
+            throw new System.NotImplementedException("No event storage connected");
+        }
+
+        public EventSet Get(int commit)
+        {
+            throw new System.NotImplementedException("No event storage connected");
+        }
+
+        public IEnumerable<EventSet> All
+        {
+            get { yield break; }
+        }
+
+        public int? LastTransactionId
+        {
+            get { return default(int?); }
+        }
+    }
+}
